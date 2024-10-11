3dcd1c71693ea9e0659e837c6a0621dd7419ef63
Gaurav <gauravjoshi20199@gmail.com>
2024-10-12 00:22:04 +0530

    Example of how to use bean annotation alongside constructor injection

---
+++ src/main/java/com/spring_revisit_latest/spring_learning/step4/preferredwayifreqcombination/AutoWiringConfig.java
@@ -0,0 +1,18 @@
+package com.spring_revisit_latest.spring_learning.step4.preferredwayifreqcombination;
+
+import com.spring_revisit_latest.spring_learning.step4.Engine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.spring_revisit_latest.spring_learning.step4"})
+public class AutoWiringConfig {
+
+    @Bean(value="marutiEngine")
+    Engine createEngine() {
+        Engine engine = new Engine();
+        engine.setEngineModelNo(124);
+        return engine;
+    }
+}
---
+++ src/main/java/com/spring_revisit_latest/spring_learning/step4/preferredwayifreqcombination/Car.java
@@ -0,0 +1,41 @@
+package com.spring_revisit_latest.spring_learning.step4.preferredwayifreqcombination;
+
+import com.spring_revisit_latest.spring_learning.step4.Engine;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+    private String brand;
+    private String model;
+    private double price;
+    private final Engine engine;
+
+    // If only one constructor then Autowired annotation is not required.
+    Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
