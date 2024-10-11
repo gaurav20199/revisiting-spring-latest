480b6911fa0a9b91782a78f4e1e5509a2b74e6d7
Gaurav <gauravjoshi20199@gmail.com>
2024-10-12 01:46:20 +0530

    Common class that is being used for bean scope examples

---
+++ src/main/java/com/spring_revisit_latest/spring_learning/step6/Car.java
@@ -0,0 +1,32 @@
+package com.spring_revisit_latest.spring_learning.step6;
+
+public class Car {
+    private String brand;
+    private String model;
+    private double price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
